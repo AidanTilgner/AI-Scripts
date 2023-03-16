803979655ca55ae2884b9b6ee37d2e09464376dc
atilgner <atilgner@digital-strata.com>
2023-03-16 15:09:54 -0700

    feat: added tag parsing

--- blogger/utils/pipeline.js
+++ blogger/utils/pipeline.js
@@ -5,6 +5,12 @@ export const pipeline = (article) => {
   let newArticle = article;
   newArticle = addToFrontMatter(newArticle, "author", cf.author.name);
   newArticle = addToFrontMatter(newArticle, "post_date", getCurrentDate());
+  const frontmatter = extractFrontMatter(newArticle);
+  newArticle = addToFrontMatter(
+    newArticle,
+    "tags",
+    checkTagsAndParseIfNecessary(frontmatter.tags)
+  );
   return newArticle;
 };
 
@@ -48,17 +54,52 @@ const addToFrontMatter = (article, key, value) => {
   return newArticleWithFrontmatter;
 };
 
-const testArticle = `
----
-title: Hello World in JavaScript
-description: Learn how to write your first JavaScript code with a "Hello World" example.
-tags: [web development, JavaScript, psilocybin therapy centers]
-test: just to see
----
+export const extractFrontMatter = (article) => {
+  const newArticle = String(article);
+  const frontmatterEndIndex = newArticle.split("\n").indexOf("---", 2);
+  const frontmatter = newArticle.split("\n").slice(2, frontmatterEndIndex);
+  const datapoints = {};
+  frontmatter.forEach((l) => {
+    const [key, value] = l.split(":");
+    if (!key || !value) return;
+    datapoints[key.trim()] = value.trim();
+  });
+  return datapoints;
+};
 
-# Hello World in JavaScript
-`;
+const checkTagsAndParseIfNecessary = async (tags) => {
+  try {
+    const parsedTags = JSON.parse(tags);
+    if (Array.isArray(parsedTags)) {
+      return parsedTags;
+    }
+  } catch (e) {
+    // we need to parse it
+    const newTags = tags
+      .replace("[", "")
+      .replace("]", "")
+      .split(",")
+      .map((t) => t.trim());
+    console.log("newTags: ", newTags);
+    return `["${newTags.join('", "')}"]`;
+  }
+};
 
-(() => {
-  console.log(pipeline(testArticle));
-})();
+// const testArticle = `
+// ---
+// title: Hello World in JavaScript
+// description: Learn how to write your first JavaScript code with a "Hello World" example.
+// tags: [web development, JavaScript, psilocybin therapy centers]
+// test: just to see
+// ---
+
+// # Hello World in JavaScript
+// `;
+
+// (async () => {
+//   console.log(extractFrontMatter(testArticle));
+//   console.log(
+//     "Checking tags: ",
+//     await checkTagsAndParseIfNecessary(extractFrontMatter(testArticle).tags)
+//   );
+// })();
